319ce0fd0e45407108d6acff9a42625a8e14dead
RKouchoo <r.kouchoo1@gmail.com>
2018-02-22 20:47:20 +1100

    initial commit of code.

---
+++ pixy_soccer/pixy_soccer.ino
@@ -0,0 +1,155 @@
+/*
+   _____________   __    _____ ___________  _____ _   _ _____ _____  ____________ _____  ___   _   __ ___________  _____
+  /  ___| ___ \ \ / /   /  __ \_   _| ___ \/  __ \ | | |_   _|_   _| | ___ \ ___ \  ___|/ _ \ | | / /|  ___| ___ \/  ___|
+  \ `--.| |_/ /\ V /    | /  \/ | | | |_/ /| /  \/ | | | | |   | |   | |_/ / |_/ / |__ / /_\ \| |/ / | |__ | |_/ /\ `--.
+  `--. \  __/ /   \    | |     | | |    / | |   | | | | | |   | |   | ___ \    /|  __||  _  ||    \ |  __||    /  `--. \
+  /\__/ / |   / /^\ \   | \__/\_| |_| |\ \ | \__/\ |_| |_| |_  | |   | |_/ / |\ \| |___| | | || |\  \| |___| |\ \ /\__/ /
+  \____/\_|   \/   \/    \____/\___/\_| \_| \____/\___/ \___/  \_/   \____/\_| \_\____/\_| |_/\_| \_/\____/\_| \_|\____/
+
+  Author: @RKouchoo
+
+*/
+
+#include <SPI.h>
+#include <Pixy.h>
+
+Pixy pixy;
+
+/**
+ * Pixy variables
+ */
+int signature = 0;
+int x = 0;                      //positon x axis
+int y = 0;                      //position y axis
+unsigned int width = 0;         //object's width
+unsigned int height = 0;        //object's height
+unsigned int area = 0;
+unsigned int newarea = 0;
+int Xmin = 70;                  //min x position
+int Xmax = 200;                 //max x position
+int maxArea = 0;
+int minArea = 0;
+
+/**
+ * Hardware variables
+ */
+int motor1 = 4;                 //motor1 on Pin D4
+int enable1 = 5;                //enable1 on Pin D5
+int motor2 = 7;                 //motor2 on Pin D7
+int enable2 = 6;                //enable2 on Pin D6
+int Speed = 70;                 //speed for motor
+static int i = 0;
+
+enum thisMotorDirection {
+  MOTOR_FORWARD,
+  MOTOR_BACKWARD,
+  MOTOR_STRAFE_LEFT,
+  MOTOR_STRAFE_RIGHT,
+  MOTOR_STOPPED
+};
+
+void setMotorDirection(thisMotorDirection thisDirection) {
+  
+}
+
+void backward()//backward
+{
+  digitalWrite(motor1, LOW);
+  digitalWrite(motor2, LOW);
+  analogWrite(enable1, Speed);
+  analogWrite(enable2, Speed);
+}
+
+void forward()//forward
+{
+  digitalWrite(motor1, HIGH);
+  digitalWrite(motor2, HIGH);
+  analogWrite(enable1, Speed);
+  analogWrite(enable2, Speed);
+}
+
+void right()//turn right
+{
+  digitalWrite(motor1, HIGH);
+  digitalWrite(motor2, LOW);
+  analogWrite(enable1, Speed);
+  analogWrite(enable2, Speed);
+}
+
+void left()//turn left
+{
+  digitalWrite(motor1, LOW);
+  digitalWrite(motor2, HIGH);
+  analogWrite(enable1, Speed);
+  analogWrite(enable2, Speed);
+}
+
+void Stop() {
+  digitalWrite(enable1, LOW);
+  digitalWrite(enable2, LOW);
+}
+
+void scanObjects() {
+  uint16_t blocks;
+  blocks = pixy.getBlocks();  //receive data from pixy
+  signature = pixy.blocks[i].signature;    //get object's signature
+  x = pixy.blocks[i].x;                    //get x position
+  y = pixy.blocks[i].y;                    //get y position
+  width = pixy.blocks[i].width;            //get width
+  height = pixy.blocks[i].height;          //get height
+}
+
+
+void setup()
+{
+  Serial.begin(9600);
+  Stop();
+  pixy.init();
+}
+
+void loop()
+{
+  while (millis() < 5000)
+  {
+    scanObjects();
+    area = width * height; //calculate the object area
+    maxArea = area + 1000;
+    minArea = area - 1000;
+  }
+
+  scanObjects();
+
+  if (signature == 2) //looking for signature 2
+  {
+    newarea = width * height; //calculate the object area
+
+    if (x < Xmin)//turn left if x position < max x position
+    {
+      left();
+    }
+    else if (x > Xmax) //turn right if x position > max x position
+    {
+      right();
+    }
+    else if (newarea < minArea) //go forward if object too small
+    {
+      forward();
+    }
+    else if (newarea > maxArea) //go backward if object too big
+    {
+      backward();
+    }
+
+    //else stop
+    else
+    {
+      Stop();
+    }
+  }
+  else
+  {
+    Stop();
+  }
+}
+
+
