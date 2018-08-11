4af2cc6fff72c6a6a16f31a83f036ed982b467c1
RKouchoo <r.kouchoo1@gmail.com>
2018-08-11 15:58:18 +1000

    removed many of the methods that are not going to be used anymore
    removed leds
    removed light sensor/photo resistor as they changed the rules a while
    ago

--- src/main.cpp
+++ src/main.cpp
@@ -8,16 +8,16 @@
   Author: @RKouchoo
   SubAuthor: @Oliver
   Emotional support: @BBlkBoi (James)
-  
+
   Last major edit:
     August 8, 2018
   Written for SPX robotics
   Written in sublime with deviot and a mix an mash of other IDE's....
 
   Robot motor layout diagram
-  
+
   1, 2, 3, 4 are the corresponding motor names in the code.
-  
+
     3   back  4
   right      left
        / - \
@@ -29,7 +29,7 @@
    - Implement gyro code.
    - Implement RF communication. (Being worked on)
    - field is spelt as feild (auto fillout)
-   
+
   RF204L01 module guide:
   https://howtomechatronics.com/tutorials/arduino/arduino-wireless-communication-nrf24l01-tutorial/
   */
@@ -42,7 +42,7 @@
 // "Custom" libraries
 #include <Pixy.h>
 #include <Adafruit_NeoPixel.h>
-#include <Adafruit_VL53L0X.h> 
+#include <Adafruit_VL53L0X.h>
 #include <nRF24L01.h>
 #include <RF24.h>
 #include <I2Cdev.h>
@@ -110,18 +110,6 @@ static int MOTOR_TWO_PWM = 1;
 static int MOTOR_THREE_PWM = 1;
 static int MOTOR_FOUR_PWM = 1;
 
-static int LIGHT_SENSOR_LEFT = 0;
-static int LIGHT_SENSOR_RIGHT = 0;
-static int LIGHT_SENSOR_FRONT = 0;
-static int LIGHT_SENSOR_BACK = 0;
-
-static int lightSensors[4] = {LIGHT_SENSOR_LEFT, LIGHT_SENSOR_RIGHT, LIGHT_SENSOR_FRONT, LIGHT_SENSOR_BACK};
-
-static int NOT_FOUND_COLOR_DATA[3] = {255, 0, 0}; // dark red
-static int FOUND_COLOR_DATA[3] = {255, 105, 180}; // hot pink
-static int REFLECTIVE_COLOR_DATA[3] = {250, 250, 210}; // At the moment this is bright yellow.
-static int LOCAL_ROBOT_ERROR_COLOR_DATA[3] = {237, 148, 90}; // error orange, should be changed later so the robot does not break the rules lol.
-
 /**
  * The arrays that collect the data for automated setup routines.
  */
@@ -129,26 +117,6 @@ static int pwms[4] = {MOTOR_ONE_PWM, MOTOR_TWO_PWM, MOTOR_THREE_PWM, MOTOR_FOUR_
 static int motors[2][4] = {
  {MOTOR_ONE[0], MOTOR_TWO[0], MOTOR_THREE[0], MOTOR_FOUR[0]},  // forward channels
  {MOTOR_ONE[1], MOTOR_TWO[1], MOTOR_THREE[1], MOTOR_FOUR[1]}  // backward channels
-};
-
-/**
- * Camera object ID's to track
- */
- #define CMYK_CYAN_GOAL_ID 2
- #define CMYK_YELLOW_GOAL_ID 3
- #define CMYK_ORGANGE_BALL_ID 1
-
-/*
- * light sensor ratios.
- */
- #define FEILD_BLACK_LINE_RATIO 50
- #define FEILD_WHITE_LINE_RATIO 50
- #define FEILD_GREEN_SURFACE_RATIO 50
-
- enum cameraTrackingObject {
-  CMYK_CYAN_GOAL,
-  CMYK_YELLOW_GOAL,
-  CMYK_ORANGE_BALL
 };
 
 enum thisMotorDirection {
@@ -173,21 +141,8 @@ enum thisRobotDirection {
   ROBOT_STRAFE_RIGHT,
   ROBOT_STRAFE_RIGHT_BACKWARD,
   ROBOT_ROTATE_RIGHT,
-  
+
   ROBOT_STOP
-};
-
-enum thisFoundFeildObject {
-  FEILD_BLACK_LINE,
-  FEILD_WHITE_LINE,
-  FEILD_GREEN_SURFACE
-};
-
-enum dualStripColor {
-  NOT_FOUND_COLOR,
-  FOUND_COLOR,
-  REFLECTIVE_COLOR,
-  LOCAL_ROBOT_ERROR_COLOR
 };
 
 /**
@@ -213,40 +168,6 @@ void initMotorConfig(int motorList[2][4]) {
 void initMotorPwmConfig(int pwmChannel[4]) {
   for (int i = 0; i < 4; i++) {
     pinMode(OUTPUT, pwmChannel[i]);
-  }
-}
-
-/**
- * routine to set up the sensor pins as an input.
- */
-void initLightSensorConfig(int sensors[4]) {
-  for (int i = 0; i < 4; i++) {
-    pinMode(INPUT, sensors[i]);
-  }
-}
-
-/**
- * returns the current value of the selected sensor.
- */
-double getLightSensorValue(int sensor) {
-  return analogRead(sensor);
-}
-
-/**
- * returns the found object below the robot.
- * Will not 
- */
-thisFoundFeildObject getCurrentFeildObject(double sensorValue) {
-  if (sensorValue <= FEILD_BLACK_LINE_RATIO) {
-    return FEILD_BLACK_LINE;
-  }
-  if (sensorValue <= FEILD_WHITE_LINE_RATIO) {
-    return FEILD_WHITE_LINE;
-  }
-  if (sensorValue <= FEILD_GREEN_SURFACE_RATIO) {
-    return FEILD_GREEN_SURFACE;
-  } else {
-    return FEILD_GREEN_SURFACE;
   }
 }
 
@@ -378,76 +299,9 @@ void setRobotDirection(thisRobotDirection robotDirection, double robotSpeed) {
   }
 }
 
-/*
- * gets the pixy to update what objects are around it.
- */
-void scanObjects(cameraTrackingObject object) {
-  int objectId;
-
-  // Choose the object to track based on what is fed in from the loop.
-  switch (object) {
-  case CMYK_CYAN_GOAL:
-    objectId = CMYK_CYAN_GOAL_ID;
-    break;
-
-  case CMYK_YELLOW_GOAL:
-    objectId = CMYK_YELLOW_GOAL_ID;
-    break;
-
-  case CMYK_ORANGE_BALL:
-    objectId = CMYK_ORGANGE_BALL_ID;
-    break;
-
-  default:
-    objectId = CMYK_ORGANGE_BALL_ID;
-    break;
-  }
-}
-
-/*
- * Updated the led strips
- */
-void updateDualStrip() {
-  dualStrip.show();
-}
-
-/*
- * Sets the led strip color with rgb values.
- */
-void setDualStripColor(int r, int g, int b) {
-  for (int i = 0; i < NEO_PIXEL_PER_ROBOT; i++) {
-    dualStrip.setPixelColor(i, r, g, b);
-    dualStrip.setBrightness(255); // Sets the brightness to full
-  }
-}
-
-/**
- * sets the color with the simpler use of an enum object.
- */
-void setDualStripColor(dualStripColor color) {
-  switch (color) {
-  case NOT_FOUND_COLOR:
-    setDualStripColor(NOT_FOUND_COLOR_DATA[0], NOT_FOUND_COLOR_DATA[1], NOT_FOUND_COLOR_DATA[2]);
-    break;
-
-  case FOUND_COLOR:
-    setDualStripColor(FOUND_COLOR_DATA[0], FOUND_COLOR_DATA[1], FOUND_COLOR_DATA[2]);
-    break;
-
-  case REFLECTIVE_COLOR:
-    setDualStripColor(REFLECTIVE_COLOR_DATA[0], REFLECTIVE_COLOR_DATA[1], REFLECTIVE_COLOR_DATA[2]);
-    break;
-
-  case LOCAL_ROBOT_ERROR_COLOR:
-    setDualStripColor(LOCAL_ROBOT_ERROR_COLOR_DATA[0], LOCAL_ROBOT_ERROR_COLOR_DATA[1], LOCAL_ROBOT_ERROR_COLOR_DATA[2]);
-    break;
-  }
-  updateDualStrip(); // push the new values to the strip's after the values have been set.
-}
-
 VL53L0X_RangingMeasurementData_t measureTOFDistance() { // gets the measurement object from the adafruit library.
   VL53L0X_RangingMeasurementData_t measurementObject;
-  timeOfFlight.rangingTest( & measurementObject, false);
+  timeOfFlight.rangingTest(&measurementObject, false);
   return measurementObject;
 }
 
@@ -459,35 +313,6 @@ double getTOFDistanceMilli(VL53L0X_RangingMeasurementData_t measurementObject) {
   } else {
     return measurementObject.RangeMilliMeter;
   }
-}
-
-float getGyroAngle() { // gets the angle that the gyro is facing
-
-  int gyro_corrected;
-  int gyro_reading;
-  float gyro_rate;
-  float accel_angle;
-  float gyro_angle;
-  int accel_reading;
-  int accel_corrected;
-
-  int16_t ax, ay, az;
-  int16_t gx, gy, gz;
-
-  gyro.getMotion6( & ax, & ay, & az, & gx, & gy, & gz);
-
-  accel_reading = ax;
-  accel_corrected = accel_reading - accel_offset;
-  accel_corrected = map(accel_corrected, -16800, 16800, -90, 90);
-  accel_corrected = constrain(accel_corrected, -90, 90);
-  accel_angle = (float)(accel_corrected * accel_scale);
-
-  gyro_reading = gy;
-  gyro_corrected = (float)((gyro_reading / 131) - gyro_offset); // 131 is sensivity of gyro from data sheet
-  gyro_rate = (gyro_corrected * gyro_scale) * -loop_time; // loop_time = 0.05 ie 50ms        
-  gyro_angle = angle + gyro_rate;
-
-  return gyro_angle;
 }
 
 void timeStamp() {
@@ -532,7 +357,7 @@ void handleRobotMovement(int speed) {
   // the ball is on the left side
   if (averageObjectX() > DEADZONE * (isCameraFlipped ? 1 : -1)) {
     if (averageObjectX() > DEADZONE_STRAFE * (isCameraFlipped ? 1 : -1)) {
-      // move backward a little then strafe left (90deg) 
+      // move backward a little then strafe left (90deg)
       setRobotDirection(ROBOT_CRAB_LEFT, speed);
     } else {
       // diag left
@@ -542,7 +367,7 @@ void handleRobotMovement(int speed) {
   } else if (averageObjectX() < DEADZONE * (isCameraFlipped ? -1 : 1)) {
     Serial.println("turn right");
     if (averageObjectX() > DEADZONE_STRAFE * (isCameraFlipped ? -1 : 1)) {
-      // move backward a little then strafe right (90deg) 
+      // move backward a little then strafe right (90deg)
       setRobotDirection(ROBOT_CRAB_RIGHT, speed);
     } else {
       // diag right
@@ -552,7 +377,7 @@ void handleRobotMovement(int speed) {
   } else {
     Serial.println("go straight");
     setRobotDirection(ROBOT_FORWARD, speed);
-    // ball should be in the centre of the dead zone pixels, 
+    // ball should be in the centre of the dead zone pixels,
     // so the robot should be able to move straight.
   }
 }
@@ -662,12 +487,12 @@ void cbRFThread() {
 
 /////////////////////////////////////////////SETUP/////////////////////////////////////////////
 void setup() {
-  // Start communication interfaces 
+  // Start communication interfaces
   Serial.begin(SERIAL_BANDWIDTH);
   Wire.begin();
 
   //// set up hardware
-  
+
   // init core devices
   pixy.init();
   gyro.initialize();
